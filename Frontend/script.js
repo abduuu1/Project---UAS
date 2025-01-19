f5836f53cea45cf3096a86d6186f0b9f401595c8
Abdullah <abduaan34@gmail.com>
2025-01-19 23:27:37 +0700

    Delete Frontend directory

--- Frontend/script.js
+++
@@ -1,6 +0,0 @@
-let screenWidth = $(window).width();
-    if (screenWidth <= 513){
-            $('.icon-menu').removeClass('col-3').addClass('col-5')
-            $('.col-menu-toogle').removeClass('col-5').addClass('col-4')
-
-        }
